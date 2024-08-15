7b9b134f15a817ec5c43e85e49ece15d92abdfd2
rhydian001 <rhydian001@gmail.com>
2024-08-15 18:52:41 +0000

    task 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 """
 Script to print hot posts on a given Reddit subreddit.
 """
@@ -24,13 +24,14 @@ def top_ten(subreddit):
     # Send a GET request to the subreddit's hot posts page
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-     # Check if the response status code indicates a not-found error (404)
- 28     if response.status_code == 404:
- 29         print("None")
- 30         return
- 31
- 32     # Parse the JSON response and extract the 'data' section
- 33     results = response.json().get("data")
- 34
- 35     # Print the titles of the top 10 hottest posts
- 36     [print(c.get("data").
+
+    # Check if the response status code indicates a not-found error (404)
+    if response.status_code == 404:
+        print("None")
+        return
+
+    # Parse the JSON response and extract the 'data' section
+    results = response.json().get("data")
+
+    # Print the titles of the top 10 hottest posts
+    [print(c.get("data").get("title")) for c in results.get("children")]
